0bf1e90a22546276625e7c783760a4ce755830cf
alexdenuke <alexdenuke@gmail.com>
2022-07-10 15:12:57 +0300

    add functional

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,32 @@
-$(document).ready(function() {
-	// $('body').hide()
-})
+let btn = document.querySelector(".dropdown__btn");
+let dropdown = document.querySelector('.dropdown');
+let dropdownBody = document.querySelector(".dropdown__body");
+let style = dropdownBody.style;
 
-// alert('hi');
+// btn.addEventListener('click', function () {
+// 	if ( dropdown.style.display === '') {
+// 		dropdown.style.display = 'block';
+// 	}
+// 	else (dropdown.style.display = '');
+// 	}
+// 	);
+
+// btn.addEventListener("click", () => {
+// 	style.display === "" ? (style.display = "block") : (style.display = "");
+// 	style.display === 'block' ? alert('hi')
+// }
+// );
+btn.addEventListener('click', () => {
+	if (style.display === '') {
+		style.display = 'block';
+	}
+	else (style.display = '');
+})
+window.addEventListener('click', (event) => {
+	target = event.target;
+	if (target == btn || target == dropdownBody) {
+		return;
+	}
+	else (style.display = '');
+
+})
