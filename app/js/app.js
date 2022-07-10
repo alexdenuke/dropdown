b5121a429de2f150c3a2923d7c4fd9aeafccd70f
alexdenuke <alexdenuke@gmail.com>
2022-07-10 18:35:11 +0300

    add ternary operator

--- app/js/app.js
+++ app/js/app.js
@@ -1,32 +1,14 @@
 let btn = document.querySelector(".dropdown__btn");
-let dropdown = document.querySelector('.dropdown');
-let dropdownBody = document.querySelector(".dropdown__body");
-let style = dropdownBody.style;
+let dropdown = document.querySelector(".dropdown__body");
+let style = dropdown.style;
 
-// btn.addEventListener('click', function () {
-// 	if ( dropdown.style.display === '') {
-// 		dropdown.style.display = 'block';
-// 	}
-// 	else (dropdown.style.display = '');
-// 	}
-// 	);
-
-// btn.addEventListener("click", () => {
-// 	style.display === "" ? (style.display = "block") : (style.display = "");
-// 	style.display === 'block' ? alert('hi')
-// }
-// );
 btn.addEventListener('click', () => {
-	if (style.display === '') {
-		style.display = 'block';
-	}
-	else (style.display = '');
-})
+	style.display == '' ? (style.display = 'flex') : (style.display = '');
+}
+);
+
 window.addEventListener('click', (event) => {
 	target = event.target;
-	if (target == btn || target == dropdownBody) {
-		return;
-	}
-	else (style.display = '');
+	target == btn || target == dropdown ? null : style.display = '';
 
 })
